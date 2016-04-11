fa563a622f7fd64eade13839892815449cb5152f
Jeremy Castagno <JeremyBYU@gmail.com>
2016-04-10 22:11:25 -0500

    Socket IO authentication is working!!!

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const app = express();
+const http = require('http').Server(app); // eslint-disable-line
+const io = require('socket.io')(http);
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
-const jwtExpress = require('express-jwt');
+const jwtExpress = require('express-jwt'); // JWT express middleware
+const socketioJwt = require('socketio-jwt'); // JWT Socket IO middleware
 const config = require('./config/config'); // get our config file
 
 app.set('jwtSecret', config.secret); // store jwt secret key
 const authenticate = jwtExpress({
   secret: config.secret,
+});
+const socketAuthenticate = socketioJwt.authorize({
+  secret: config.secret,
+  timeout: 15000, // 15 seconds to send the authentication message
 });
 
 const orm = require('./models/orm').orm;
@@ -22,34 +29,46 @@ app.use(bodyParser.urlencoded({
 // Server static files
 app.use(express.static('public'));
 
-// Error Handling
-
 const indexRoutes = require('./routes/index');
 const apiRoutes = require('./routes/api');
 
+// Set Routes
+app.use('/', indexRoutes);
+app.use('/api', authenticate, apiRoutes);
+// Error Handler
+app.use('/api', (err1, req, res, next) => {
+  if (err1.name === 'UnauthorizedError' || 'jwt') {
+    res.status(401).json({
+      success: false,
+      message: 'Authorization failed',
+    });
+  }
+  next();
+});
+// Initializing connection to 'database', which is just a file in this case
+// db file should be in the .tmp folder.
 orm.initialize(config.ormConfig, (err, models) => {
   if (err) {
-    return console.error(err);
+    console.error(err);
   }
-  // Tease out fully initialised models.
+  // Tease out fully initialised models and set to app instance
   app.models = models.collections;
   // Intialize First User (Jeremy, password)
   utils.initializeUser(app.models.user);
-
-  // Set Routes
-  app.use('/', indexRoutes);
-  app.use('/api', authenticate, apiRoutes);
-  // Error Handler
-  app.use('/api', (err1, req, res, next) => {
-    if (err.name === 'UnauthorizedError' || 'jwt') {
-      res.status(401).json({ success: false, message: 'Authorization failed' });
-    }
-    next();
-  });
-
   // Start the server
-  app.listen(config.port);
+  http.listen(config.port);
   console.log('Magic happens at http://localhost:' + config.port);
-
-  return true;
 });
+
+// Why dont I see any errors for socket io when the token is invalid?
+// set authorization for socket.io
+io
+  .on('connection', socketAuthenticate)
+  .on('authenticated', (socket) => {
+    // this socket is authenticated, we are good to handle more events from it.
+    console.log('hello! ' + socket.decoded_token.firstName);
+    socket.on('message', (message) => {
+        // this socket is authenticated, we are good to handle more events from it.
+        console.log(`Message from ${socket.decoded_token.firstName}: ${message}`);
+    });
+  });
--- public/app.js
+++ public/app.js
@@ -1,7 +1,9 @@
-/* global $,var2*/
+/* global $,io*/
 'use-strict';
+var socket = undefined;
 $(document).ready(() => {
-  $('#submitBtn').click(() => {
+  $('#submitBtn').click((e) => {
+    e.preventDefault();
     var firstName = $('#firstName').val();
     var password = $('#password').val();
     $.ajax({
@@ -13,7 +15,6 @@ $(document).ready(() => {
       }
     })
     .done((msg) => {
-      alert("Data Saved: " + JSON.stringify(msg));
       if (msg.success) {
         $('#token').val(msg.token);
       } else {
@@ -21,4 +22,32 @@ $(document).ready(() => {
       }
     });
   });
+
+  $('#startSocket').click((e) => {
+    e.preventDefault();
+    beginSocketConnection($('#token').val());
+  });
+
+  $('#socketBtn').click((e) => {
+    e.preventDefault();
+    var message = $('#socket').val();
+    if (socket) {
+      console.log('trying to send message');
+      socket.emit('message', message);
+    } else {
+      alert('No socket Connection');
+    }
+  });
 });
+
+function beginSocketConnection(token) {
+  socket = io.connect('http://localhost:8080');
+  socket.on('connect', () => {
+    socket.on('authenticated', () => {
+      $('#socket').val('Im authenticated using jwt for socket io!. You can now type things in here and broadcast messages!!');
+      console.log('authentiacted!');
+    })
+    .emit('authenticate', { token: token }); //send the jwt
+    console.log('Trying to authenticate...');
+  });
+}
